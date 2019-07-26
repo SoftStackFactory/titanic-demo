14181d409dc26aaa43aeb6bf25d10b324d150922
David Arias <davidalexarias2@gmail.com>
2019-07-26 12:09:15 -0700

    removed mock data and added comments throughout app.py

--- app.py
+++ app.py
@@ -3,60 +3,47 @@ from sklearn.externals import joblib
 import pickle
 import numpy as np
 
+# Create Flask App
 app = Flask(__name__)
 
 
+# Home Route
 @app.route('/')
 def hello_world():
     return jsonify({"what":'Hello World...'})
 
 
-
+# Prediction Route
 @app.route('/predict', methods = ['GET', 'POST'])
 def process_data():
 
 # Grab JSON input
     input_data = request.get_json()
     
-
 # Initiate Variables
     age = input_data['age']
     gender = input_data['gender']
     was_alone = input_data['was_alone']
 
+# Load Model
     model = joblib.load('./titanic_grad_boost.joblib')
 
-
-# mock data
-    mock_data = {
-        'age': 38,
-        'was_alone': 1,
-        'gender': 1
-    }
-
-# process
-    mock_input = [mock_data['age'], mock_data['was_alone'], mock_data['gender']]
+# process data as model input
     input_data = [age, was_alone, gender]
     input_data = np.array(input_data).reshape(1, -1)
-    # print(input_data)
 
-# predict
+# Make prediction 
     pred = model.predict(input_data)[0]
     prob_of_survival = model.predict_proba(input_data)[0][1]
 
-# result
-    if pred == 1:
-        result = 'survived'
-    else:
-        result = 'perished'
-
-    # result = 'survived' if pred == 1 else 'perished'
-
+# Process results
+    result = 'survived' if pred == 1 else 'perished'
     chance_of_survival= round(prob_of_survival, 2)
 
-
+# Return Payload
     return jsonify({"result":result, "chance":chance_of_survival})
 
 
+# Run app 
 if __name__ == '__main__':
     app.run(host= '0.0.0.0') #run app in debug mode on port 5000
